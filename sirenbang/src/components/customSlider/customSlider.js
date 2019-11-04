26e163c36aa0ed0318803607e5966a968c89cf5b
OnlyChestnut <905859017@qq.com>
2019-11-04 21:15:02 +0800

    lxl

---
+++ sirenbang/src/components/customSlider/customSlider.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {withRouter} from 'react-router-dom'
+import { Menu, Icon } from 'antd';
+import webStorage from '../../utils/webstorage'
+const { SubMenu } = Menu;
+
+
+const root=webStorage.getItem('rootList')
+class CustomSlider extends React.Component{
+    jump=(path)=>{
+        this.props.history.push(path)
+    }
+
+    renderItem=(data)=>{
+        //1、判断有没有children, 有=>sub  没有=>item
+        return data.map((item,index)=>{
+            if(item.subprime){
+                //渲染次级
+                return(
+                    <SubMenu title={item.name}>
+                        {this.renderItem(item.subprime)}
+                    </SubMenu>
+                )
+            }else{
+                return(
+                    <Menu.Item key={item.key} onClick={this.jump.bind(this,item.path)}>{item.name}</Menu.Item>
+                )
+            }
+        })
+    }
+
+    render(){
+        return(
+            <Menu style={{ width: 256 }} mode="vertical" theme='dark'>
+                {this.renderItem(root)}
+
+            </Menu>
+        )
+    }
+}
+export default withRouter(CustomSlider)
--- sirenbang/src/pages/admin/admin.js
+++ sirenbang/src/pages/admin/admin.js
@@ -1,17 +1,57 @@
 import React from 'react'
+// import Style from './index.module.less'
+// import Modal from '../../components/modal/modal'
+import { Layout, Menu, Icon,Dropdown,Button } from 'antd';
 import {withRouter} from 'react-router-dom'
+import CustomSlider from '../../components/customSlider/customSlider'
+const { Header, Content, Footer, Sider } = Layout;
+
 class Admin extends React.Component{
-  render(){
-    return(
-      <div>
-        这里是主页admin
-        <button onClick={()=>{
-            this.props.history.push('/login')
-        }}>去登陆页面</button>
-            
-        {this.props.children}
-      </div>
-    )
-  }
+    renderMenu=()=>{
+        return (
+            <Menu>
+                <Menu.Item onClick={this.logout}>
+                    <span>用户注销</span>
+                </Menu.Item>
+
+            </Menu>
+        );
+    }
+    logout=()=>{
+        this.props.history.replace('/login')
+    }
+
+    render(){
+        return(
+            <Layout>
+                <Sider
+                    style={{
+                        overflow: 'auto',
+                        height: '100vh',
+                        position: 'fixed',
+                        left: 0,
+                    }}
+                >
+                    <CustomSlider></CustomSlider>
+
+                </Sider>
+                <Layout style={{ marginLeft: 200 }}>
+                    <Header style={{ background: '#fff', padding: 0 }}>
+                        <Dropdown overlay={this.renderMenu} placement="bottomLeft">
+                            <Button>用户信息</Button>
+                        </Dropdown>
+                    </Header>
+                    <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+                        <div style={{ padding: 24, background: '#fff', textAlign: 'center' }}>
+                            {this.props.children}
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+                </Layout>
+                {/*<Modal></Modal>*/}
+            </Layout>
+        )
+    }
 }
-export default withRouter(Admin);
+
+export default withRouter(Admin)
