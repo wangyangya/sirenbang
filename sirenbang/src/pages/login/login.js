67335f97918c108607041229cf3fc3699bd0d5b2
xiaocuiba <1205016358@qq.com>
2019-11-04 21:26:11 +0800

    cjl4

--- sirenbang/src/pages/login/login.js
+++ sirenbang/src/pages/login/login.js
@@ -1,8 +1,37 @@
 import React from 'react'
 import { Form, Icon, Input, Button, Checkbox ,Card} from 'antd';
 import Style from './login.module.less'
-import withRouter from 'react-router-dom'
 class Login extends React.Component{
+    submit=()=>{
+
+        //console.log(this.state)
+        // getFieldsValue 获取双向绑定的数据，没有校验效果
+        //let result=this.props.form.getFieldsValue();
+        this.props.form.validateFields((err,userinfo)=>{
+            //err验证是不是通过
+            if (err){
+                message.error('输入信息有误请重试')
+            } else{
+                this.$axios.post('/haha/v1/admin/user/login',userinfo)
+                    .then((data)=>{
+                        console.log(data)
+                        if(data.err===0){
+                            //存值
+                            webStorage.setItem('rootList',data.rootList)
+                            webStorage.setItem('token',data.token)
+                            webStorage.setItem('uid',data.uid)
+                            //跳转到首页
+                            console.log('跳转')
+                            this.props.history.push('/admin')
+                        }else {
+
+                        }
+                    })
+                message.success('登录成功，1后跳转首页')
+            }
+        })
+        //console.log(result)
+    }
   render(){
       const { getFieldDecorator } = this.props.form;
       return (
