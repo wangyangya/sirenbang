63989e813ddd36b3dbcc564b9d9aab84fc9133a7
yang1033821149 <1033821149@qq.com>
2019-11-06 20:38:02 +0800

    yyc-updata-imgbug

--- sirenbang/src/pages/food/foodlist.js
+++ sirenbang/src/pages/food/foodlist.js
@@ -1,5 +1,7 @@
 import React ,{Component,Fragment}from 'react';
-import {Table,Card,Pagination} from 'antd';
+import {Table,Card,Pagination, message} from 'antd';
+import UpData from './upData'
+
 /*
 1.查看所有管理员信息
   a.请求数据
@@ -26,14 +28,25 @@ class Rootlist extends Component{
         key: 'cc',
     },
     {
-        title: '新品种类',
+        title: '新品类别',
         dataIndex: 'typename',
         key: 'dd',
     },
     {
+        title: '新品种类',
+        dataIndex: 'typeid',
+        key: 'ee',
+        render:(data)=>{
+            let obj=['种类一','种类二','种类三','种类四','种类五','种类六','种类七'];
+            return(<span>
+                {obj[data-1]}
+            </span>)
+        }
+    },
+    {
         title: '新品图片',
         dataIndex: 'img',
-        key: 'ee',
+        key: 'ff',
         render:(data)=>{
             return(
                 <div>
@@ -46,11 +59,14 @@ class Rootlist extends Component{
     {
       title: '操作',
       key: 'action',
-      render:()=>{
+      render:(data)=>{
         return(
           <Fragment>
                 <button>删除</button>
-              <button>修改</button>
+              <button onClick={()=>{
+                 this.setState({isShow:true,upData:data})
+              }
+              }>修改</button>
           </Fragment>
         )
       }
@@ -62,7 +78,9 @@ class Rootlist extends Component{
             dataSource:[],
             page:1,
             pageSize:2,
-            total:0
+            total:0,
+            isShow:false,
+            upData:{}
 
         }
     }
@@ -73,17 +91,34 @@ class Rootlist extends Component{
     getDoodlist=(page,pageSize)=>{
         this.$axios.post('./hehe/food/getInfoByPage',{page,pageSize})
         .then((data)=>{
-            console.log(data)
+            // console.log(data)
            if(data.err===0){
-               this.setState({dataSource:data.info.list,total:data.info.count})
+               this.setState({dataSource:data.info.list,total:data.info.count,page:page})
            }
         })
     }
     pageChang=(page,pageSize)=>{
         this.getDoodlist(page,pageSize)
     }
+    //修改页面的开关
+    isShow=(bool)=>{
+        this.setState({isShow:bool})
+    }
+    //修改数据
+    submit=(name,price,desc,typename,typeid,img,_id)=>{
+        let {page,pageSize}=this.state
+        this.isShow(false)
+        this.$axios.post('hehe/food/update',{name,price,desc,typename,typeid,img,_id})
+        .then((data)=>{
+           if(data.err===0){
+            message.success('修改成功了')
+            this.getDoodlist(page,pageSize)
+           }
+        })
+       
+    }
     render(){
-        let {dataSource,page,pageSize,total}=this.state
+        let {dataSource,page,pageSize,total,isShow,upData}=this.state
         return(
             <div>
                <Card title="菜单管理列表">
@@ -97,7 +132,7 @@ class Rootlist extends Component{
                      onChange={this.pageChang}
                  />
               </Card>
-                
+              {isShow&&<UpData upData={upData} isShow={this.isShow} submit={this.submit}></UpData>}
             </div>
         )
     }
